9a8eb09df7a081b62c5a7e0aa04111b21f362c0f
Jose <jsanchezl9@miumg.edu.gt>
2026-09-17 19:16:10 -0600

    Ajustes a modulo de libros

--- database.py
+++ database.py
@@ -3,12 +3,20 @@ from mysql.connector import Error
 
 def get_connection():
     try:
+        # connection = mysql.connector.connect(
+        #     host="localhost",
+        #     port=1106,
+        #     user="seminario",
+        #     password="seminario123",
+        #     database="biblio_uni"
+        # )
+
         connection = mysql.connector.connect(
-            host="localhost",
-            port=1106,
-            user="seminario",
-            password="seminario123",
-            database="biblio_uni"
+            host="sql5.freesqldatabase.com",
+            port=3306,
+            user="sql5837287",
+            password="Ffx6BuTmCE",
+            database="sql5837287"
         )
 
         return connection
